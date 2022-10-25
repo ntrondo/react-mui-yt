a78517bfdc8b2e96c4027a5ff09397e0701fc4ec
Christopher Willoch <ulveie@msn.com>
2022-10-25 11:50:07 +0200

    Navbar with logo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # react-mui-yt
 Youtube clone built with react.js , material ui and ..api..
 Tutorial: https://www.youtube.com/watch?v=FHTbsZEJspU
-Current commit: 20 min 03 sec.
+Current commit: 30 min 50 sec.
--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {Box} from '@mui/material'
-import Navbar from './components/Navbar'
-import Feed from './components/Feed'
-import VideoDetail from './components/VideoDetail'
-import ChannelDetail from './components/ChannelDetail'
-import SearchFeed from './components/SearcgFeed'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
+
 
 const App = () => (
   <BrowserRouter>
-  <Box sx={{backgroundColor:'#000'}}>
-    <Navbar />
-    <Routes>
-        <Route path="/" exact element={<Feed />}/>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
-    </Routes>
-  </Box>
+      </Routes>
+    </Box>
   </BrowserRouter>
 )
 
